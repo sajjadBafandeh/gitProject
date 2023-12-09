bd0fc453aabba092a41c018ff7880610bf5e375b
sajjad bafandeh <sajjad.bafandeh.9799@gmail.com>
2023-12-09 18:51:49 +0330

    update gitignore and index

--- index.php
+++ index.php
@@ -12,3 +12,8 @@ require "vendor/autoload.php";
 use App\Controller\HomeController;
 $rig = new HomeController();
 $rig->index('sajjad bafandeh');
+
+
+$e = new \GuzzleHttp\Client();
+$data = $e->request("GET" , "https://roocket.ir/series/learn-composer/episode/18");
+echo $data->getBody();
